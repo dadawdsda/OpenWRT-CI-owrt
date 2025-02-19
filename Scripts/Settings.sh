ddfd702bcb971edc089eca351f29c9dc53c47aed
dadawdsda <13465198288@163.com>
2025-02-19 20:46:58 +0800

    添加修改默认源的脚本

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -63,3 +63,18 @@ if [[ $WRT_TARGET == *"QUALCOMMAX"* ]]; then
 	sed -i 's/KERNEL_SIZE := 6144k/KERNEL_SIZE := 12288k/' ./target/linux/qualcommax/image/ipq60xx.mk
 
 fi
+
+emortal_def_dir="./package/emortal/default-settings"
+distfeeds_conf="$emortal_def_dir/files/99-distfeeds.conf"
+
+if [ -d "$emortal_def_dir" ] && [ ! -f "$distfeeds_conf" ]; then
+        install -Dm755 "$GITHUB_WORKSPACE/patches/99-distfeeds.conf" "$distfeeds_conf"
+
+        sed -i "/define Package\/default-settings\/install/a\\
+\\t\$(INSTALL_DIR) \$(1)/etc\\n\
+\t\$(INSTALL_DATA) ./files/99-distfeeds.conf \$(1)/etc/99-distfeeds.conf\n" $emortal_def_dir/Makefile
+
+        sed -i "/exit 0/i\\
+[ -f \'/etc/99-distfeeds.conf\' ] && mv \'/etc/99-distfeeds.conf\' \'/etc/opkg/distfeeds.conf\'\n\
+sed -ri \'/check_signature/s@^[^#]@#&@\' /etc/opkg.conf\n" $emortal_def_dir/files/99-default-settings
+    fi
